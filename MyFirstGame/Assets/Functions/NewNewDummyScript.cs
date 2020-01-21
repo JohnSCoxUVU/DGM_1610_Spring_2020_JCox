d2833b19206baa258f8ab1d2658b6fa391d5827a
John Cox <jscox1997@gmail.com>
2020-01-20 21:34:58 -0700

    Scripting practice

---
+++ MyFirstGame/Assets/Functions/NewNewDummyScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewNewDummyScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int uno;
+        uno = 1;
+        int dos;
+        dos = 2;
+        int tres;
+        tres = 3;
+        int quatro;
+        quatro = 4;
+        int cinco;
+        cinco = 5;
+        int ses;
+        ses = 6;
+        int siete;
+        siete = 7;
+        int ocho;
+        ocho = 8;
+        int nueve;
+        nueve = 9;
+        int dias;
+        dias = 10;
+        int total
+        total = uno + dos + tres + quatro + cinco + ses + siete + ocho + nueve + dias;
+        Debug.Log(total);
+        int Idontknoweleveninspanish;
+        Idontknoweleveninspanish = 11;
+        int newtotal;
+        newtotal = total + Idontknoweleveninspanish;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
