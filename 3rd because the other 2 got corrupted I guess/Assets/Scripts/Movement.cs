f04e1d690a4e7584008381297d50a9811ffe39e2
John <jscox1997@gmail.com>
2020-01-27 17:13:04 -0700

    Trigger events

--- 3rd because the other 2 got corrupted I guess/Assets/Scripts/Movement.cs
+++ 3rd because the other 2 got corrupted I guess/Assets/Scripts/Movement.cs
@@ -15,7 +15,37 @@ public class Movement : MonoBehaviour
     {
 
         transform.Translate(0, 0, 0.1f);
+                            // (x,y,z)
+
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+
+
+        if (other.gameObject.CompareTag("Floor")) //primary 
+        {
+            Debug.Log("Colliding with Floor");
+        }
+        else if(other.gameObject.CompareTag("Obstacle")) //secondary c:
+        {
+            Debug.Log("Colliding with Obstacle");
+        }
+        else //default/tertiary
+        {
+            Debug.Log("...");
+        }
+
 
 
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Trigger activated");
+
+
+    }
+
+
 }
