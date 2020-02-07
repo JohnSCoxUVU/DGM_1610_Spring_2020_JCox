ee66254dc9cbef04012b0b373dfee11d478c6d12
John Cox <jscox1997@gmail.com>
2020-02-06 23:02:04 -0700

    Succeeded on making a jump script!

---
+++ 3rd because the other 2 got corrupted I guess/Assets/Scripts/Jumpmaybe.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+public class Jumpmaybe : MonoBehaviour
+{
+    public int forceConst = 50;
+
+private bool canJump;
+    private Rigidbody selfRigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        selfRigidbody = GetComponent<Rigidbody>();
+
+    }
+
+    void FixedUpdate()
+    {
+        if (canJump)
+        {
+            canJump = false;
+            selfRigidbody.AddForce(0, forceConst, 0, ForceMode.Impulse);
+
+        }    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            canJump = true;
+
+        }
+    }
+}
