fb13382ac96871bd4c5b7122733464ffe3b74940
John Cox <jscox1997@gmail.com>
2020-02-18 23:10:56 -0700

    Used a reference example script to attempt to figure out ammo bar

---
+++ 3rd because the other 2 got corrupted I guess/Assets/Scripts/AmmoBarAttempt.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmmoBarAttempt : MonoBehaviour
+{
+    public int ammo = 30;
+    public Text uitext;
+    private bool isAttacking = false;
+    public float delay = 0.1f;
+
+    void Update()
+    {
+
+        if (Input.GetButton("Fire1"))
+        {
+            StartCoroutine(Attack());
+        }
+
+    }
+
+    private IEnumerator Attack()
+    {
+        if (isAttacking == true)
+        {
+            yield break;
+        }
+        isAttacking = true;
+        ammo -= 1;
+        if (uitext != null)
+        {
+            uitext.text = ammo.ToString();
+        }
+        yield return new WaitForSeconds(delay);
+        isAttacking = false;
+    }
+}
+
