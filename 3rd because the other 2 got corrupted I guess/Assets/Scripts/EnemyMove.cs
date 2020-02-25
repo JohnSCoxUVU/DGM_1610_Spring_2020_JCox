cf8c431566ecddc492769cb53c64147481eb40cc
John <jscox1997@gmail.com>
2020-02-24 17:10:07 -0700

    New form of enemy movement

--- 3rd because the other 2 got corrupted I guess/Assets/Scripts/EnemyMove.cs
+++ 3rd because the other 2 got corrupted I guess/Assets/Scripts/EnemyMove.cs
@@ -4,26 +4,35 @@ using UnityEngine;
 
 public class EnemyMove : MonoBehaviour
 {
-    public GameObject player;
-    public Transform target;
+        // public Transform target;
     public float moveSpeed;
-    public int damage;
-
+    private Rigidbody enemyRb;
+    public GameObject player;
 
     // Start is called before the first frame update
     void Start()
     {
+        // target = GameObject.Find("Player").transform;
+        enemyRb = GetComponent<Rigidbody>();
         player = GameObject.Find("Player");
-
-
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.LookAt(target);
-        transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+       // transform.LookAt(target);
+        //transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
 
 
     }
+
+   void FixedUpdate()
+    {
+        enemyRb.AddForce((player.transform.position - transform.position).normalized * moveSpeed);    
+    }
+
+
 }
+
+
+
