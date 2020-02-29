0be880fb851de340c98b597994bb2ebc87df78f5
John Cox <jscox1997@gmail.com>
2020-02-28 19:40:06 -0700

    Attempted to fix absence of jumping

--- 3rd because the other 2 got corrupted I guess/Assets/Scripts/OptimizedMovement.cs
+++ 3rd because the other 2 got corrupted I guess/Assets/Scripts/OptimizedMovement.cs
@@ -8,8 +8,8 @@ public class OptimizedMovement : MonoBehaviour
    
     public float speed = 12f;
     public float gravity = -9.81f;
-
-   
+    public float jumpforce = 10.0f;
+    private Vector3 motion = Vector3.zero;
 
 
     // Update is called once per frame
@@ -20,5 +20,28 @@ public class OptimizedMovement : MonoBehaviour
 
         Vector3 move = transform.right * x + transform.forward * z;
         controller.Move(move * speed * Time.deltaTime);
+
+                 }
+    void Move()
+    {
+        motion = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        motion *= speed;
+        motion = transform.TransformDirection(motion);
+
+        controller.Move(motion);
+        //controller.SimpleMove(motion);
+
+        //jump
+        if (Input.GetKeyDown(KeyCode.Space) & controller.isGrounded)
+        {
+            motion.y = jumpforce;
+        }
+
+        motion.y -= gravity * Time.deltaTime;
+        controller.Move(motion * Time.deltaTime);
+
     }
+
+
+
 }
