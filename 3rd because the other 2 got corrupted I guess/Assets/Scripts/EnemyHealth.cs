1af25b43c5192b4ca9a6b1a62c6f4c2c4fa0312a
John <jscox1997@gmail.com>
2020-03-02 17:11:16 -0700

    Enemy Health + Bullet scripts

---
+++ 3rd because the other 2 got corrupted I guess/Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+
+    public int CurrentHealth;
+    public int maxHealth = 3;
+    public Transform SpawnPoint;
+    public int points = 10;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CurrentHealth = maxHealth;
+        SpawnPoint = GameObject.Find("SpawnPoint").transform;
+    }
+
+    // Update is called once per frame
+    public void TakeDamage(int amount)
+    {
+        CurrentHealth -= amount;
+        if (CurrentHealth <= 0)
+        {
+            CurrentHealth = 0;
+            print("Enemy is Dead!");            
+            // Add points to score for killing enemy
+            //scoreManager.AddPoints(points);
+            //Move enemy to spawn point for restart
+            transform.position = SpawnPoint.position;
+            transform.rotation = SpawnPoint.rotation;
+            CurrentHealth = maxHealth;
+        }
+    }
+}
