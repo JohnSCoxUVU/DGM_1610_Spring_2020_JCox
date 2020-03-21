abfd65361f9bdbde8e563acd758c230d36fa4f90
John Cox <jscox1997@gmail.com>
2020-03-20 23:43:23 -0600

    melee attack

---
+++ 3rd because the other 2 got corrupted I guess/Assets/Scripts/meleeattack.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class meleeattack : MonoBehaviour
+{
+    public float damage = 50.0F;
+    public float attackDuration = 0.3F;
+
+    public bool attacking = false;
+
+    [HideInInspector]
+
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+
+        if (Input.GetKeyDown("h"))
+        {
+            attacking = true;
+        }
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+
+        if (col.tag == "Enemy")
+        {
+            if (attacking)
+            {
+                col.SendMessage("receiveDamage", damage, SendMessageOptions.DontRequireReceiver);
+            }
+        }
+    }
+
+    void EnableDamage()
+    {
+        if (attacking == true) return;
+        attacking = true;
+        StartCoroutine("DisableDamage");
+    }
+
+    IEnumerator DisableDamage()
+    {
+        yield return new WaitForSeconds(attackDuration);
+        attacking = false;
+    }
+}
