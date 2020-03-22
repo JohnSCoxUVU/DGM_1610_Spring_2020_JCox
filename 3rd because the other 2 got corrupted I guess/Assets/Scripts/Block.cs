fc1e30ace697136971e4a8ae1e51ab52eda7de62
John Cox <jscox1997@gmail.com>
2020-03-21 22:17:46 -0600

    Blocking

---
+++ 3rd because the other 2 got corrupted I guess/Assets/Scripts/Block.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour
+{
+    public GameObject shield;
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+
+        if (col.gameObject.tag == "Shield")
+        {
+            SpawnShield();
+            Destroy(GameObject.FindGameObjectWithTag("Shield"));
+        }
+    }
+
+    public void SpawnShield()
+    {
+        Instantiate(shield, GetComponent<OptimizedMovement>().gameObject.transform.position,
+                    GetComponent<OptimizedMovement>().gameObject.transform.rotation);
+    }
+}
+
+
