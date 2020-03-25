431539bd5d2db18871c8a14ca6ed48ce30e8245a
John Cox <jscox1997@gmail.com>
2020-03-25 17:22:46 -0600

    Enums and switches!

---
+++ 3rd because the other 2 got corrupted I guess/Assets/Scripts/Enumpractice.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enumpractice : MonoBehaviour
+{
+
+
+    enum Days {sun = 1, mon = 2, tues, wed, thurs, fri, sat };
+
+    Days currentDay;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentDay = Days.sun;
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(currentDay == Days.mon)
+        {
+            print("Go back to bed!");
+        }
+
+
+    }
+}
---
+++ 3rd because the other 2 got corrupted I guess/Assets/Scripts/Switchesgetstitches.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Switchesgetstitches : MonoBehaviour
+{
+
+    public string favHero;
+    public int smarts = 5;
+
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (favHero == "Superman")
+        {
+            print("Wrong!");
+        }
+        else if (favHero == "Thor") {
+            print("Noice!");
+                }
+
+        switch (favHero)
+        {
+            case "Superman":
+                print("Wrong!");
+                break;
+            case "Thor":
+                print("Correct!");
+                break;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (smarts)
+        {
+
+        }
+
+
+
+
+    }
+}
