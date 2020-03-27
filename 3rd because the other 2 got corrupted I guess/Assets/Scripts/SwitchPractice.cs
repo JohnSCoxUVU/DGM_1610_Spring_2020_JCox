2451d13196a652861faf4fbcd89e5e48aef0c125
John Cox <jscox1997@gmail.com>
2020-03-26 22:33:44 -0600

    Switch practice

---
+++ 3rd because the other 2 got corrupted I guess/Assets/Scripts/SwitchPractice.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchPractice : MonoBehaviour
+{
+    public string favCandy;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (favCandy == "Chocolate")
+        {
+            print("Nice!");
+        }
+
+
+        switch (favCandy)
+        {
+            case "Chocolate":               
+                print("Nice!");
+                break;
+            case "Caramel":
+                print("Acceptable.");
+                break;
+            case "Licorice":
+                print("Nope.");
+                break;
+            case "I don't like candy":
+                print("To each their own, friend.");
+                break;
+
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
