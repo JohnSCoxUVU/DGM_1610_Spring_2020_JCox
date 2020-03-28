88bc8fd8cfa7088873efd92da4d0b59afc9f3860
John Cox <jscox1997@gmail.com>
2020-03-27 18:12:51 -0600

    Health and Damage

---
+++ 3rd because the other 2 got corrupted I guess/Assets/Scripts/Player Health.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int playerHealth;
+    public int damage;
+    public bool dead;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerHealth = 100;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerHealth <= 0)
+        {
+            dead = true;
+        }
+        else
+        {
+            dead = false;
+        }
+
+        if (dead == true)
+        {
+            print("GAME OVER");
+        }
+    }
+}
