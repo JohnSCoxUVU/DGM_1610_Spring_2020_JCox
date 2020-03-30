b2f5a0708151df67e702b178d89d6e49bc318467
John Cox <jscox1997@gmail.com>
2020-03-30 17:26:47 -0600

    UI elements and scoremanager

--- 3rd because the other 2 got corrupted I guess/Assets/Scripts/ScoreManager.cs
+++ 3rd because the other 2 got corrupted I guess/Assets/Scripts/ScoreManager.cs
@@ -1,23 +1,51 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
 
 public class ScoreManager : MonoBehaviour
 {
     public static int score;
-    
+    public int winScore;
+
+    public Text winText;
+    Text scoreText;
+
+
+    private void Awake()
+    {
+        Time.timeScale = 1;
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        scoreText = GetComponent<Text>();
+        score = 0;
+        winText.GetComponent<Text>().enabled = false;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-        
+        if (score < 0)
+            score = 0;
+        scoreText.text = " " + score;
+
+        if(score >= winScore)
+        {
+            print("Win score reached = " + score);
+            winText.GetComponent<Text>().enabled = true;
+            Time.timeScale = 0;
+        }
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            SceneManager.LoadScene(0);
+        }
     }
 
     public static void AddPoints(int pointsToAdd)
