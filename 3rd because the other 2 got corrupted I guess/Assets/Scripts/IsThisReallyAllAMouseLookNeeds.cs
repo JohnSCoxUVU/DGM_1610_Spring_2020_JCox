601e15dbfc88f3ae4eee6c407f3cc967ea865bd5
John Cox <jscox1997@gmail.com>
2020-04-01 21:48:45 -0600

    MouseLook???

---
+++ 3rd because the other 2 got corrupted I guess/Assets/Scripts/IsThisReallyAllAMouseLookNeeds.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IsThisReallyAllAMouseLookNeeds : MonoBehaviour
+{
+
+
+    // Update is called once per frame
+    public Transform player;
+
+    void Update()
+    {
+        transform.position = player.transform.position;
+    }
+}
+
