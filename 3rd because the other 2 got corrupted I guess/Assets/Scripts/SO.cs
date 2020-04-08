37be5090daee8f161c84a9a75b7beed31440a303
John Cox <jscox1997@gmail.com>
2020-04-08 17:17:22 -0600

    Scriptable objects

---
+++ 3rd because the other 2 got corrupted I guess/Assets/Scripts/SO.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName ="New SO",menuName ="Scriptable Object")]
+
+public class SO : ScriptableObject
+{
+
+    public new string name;
+   
+    public string description;
+
+
+    public GameObject model;
+
+    public Color color;
+
+
+
+
+
+
+
+}
