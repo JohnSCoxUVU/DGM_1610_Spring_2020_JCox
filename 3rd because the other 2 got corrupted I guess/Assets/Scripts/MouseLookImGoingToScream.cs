fa9b83f4ee034ce0535a9cac50ce710e197ddcc5
John Cox <jscox1997@gmail.com>
2020-04-16 21:29:38 -0600

    Made flashlight follow view

---
+++ 3rd because the other 2 got corrupted I guess/Assets/Scripts/MouseLookImGoingToScream.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLookImGoingToScream : MonoBehaviour
+{
+    [SerializeField] private float mouseSensitivity;
+    private float xAxisClamp;
+
+    [SerializeField] private Transform playerBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CameraRotation();
+    }
+
+    private void CameraRotation()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+       
+        //inputs and rotation
+
+
+        xAxisClamp += mouseY;
+
+        if (xAxisClamp > 90.0f)
+        {
+            xAxisClamp = 90.0f;
+            mouseY = 0.0f;
+            ClampXAxisRotationToValue(270.0f);
+        } 
+        else if (xAxisClamp < -90.0f)
+        {
+            xAxisClamp = -90.0f;
+            mouseY = 0.0f;
+            ClampXAxisRotationToValue(90.0f);
+        }
+        //Caps how high or low the player can look     
+
+        transform.Rotate(Vector3.left * mouseY);
+        playerBody.Rotate(Vector3.up * mouseX);
+
+    }
+
+    private void ClampXAxisRotationToValue(float value)
+    {
+        Vector3 eulerRotation = transform.eulerAngles;
+        eulerRotation.x = value;
+        transform.eulerAngles = eulerRotation;
+    }
+    //measures rotation values for the vertical look cap
+
+
+}
---
+++ 3rd because the other 2 got corrupted I guess/Assets/Scripts/MovementOnceMoreWithFeeling.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+
+public class MovementOnceMoreWithFeeling : MonoBehaviour
+{
+    private CharacterController charController;
+    [SerializeField] private float movementSpeed;
+
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        charController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMovement();
+    }
+
+    private void PlayerMovement()
+    {
+        float vertInput = Input.GetAxis("Vertical") * movementSpeed;
+        float horInput = Input.GetAxis("Horizontal") * movementSpeed;
+
+       UnityEngine.Vector3 forwardMovement = transform.forward * vertInput;
+        UnityEngine.Vector3 sidewaysMovement = transform.right * horInput;
+
+        charController.SimpleMove(forwardMovement + sidewaysMovement);
+        //Time.deltatime not necessary, simplemove does so automatically
+    }
+
+}
